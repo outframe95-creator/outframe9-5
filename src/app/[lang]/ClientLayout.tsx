6403d1ca1f23810cd5ea43c46bcbd1f53ef0fb6e
outframe95-creator <outframe95@gmail.com>
2026-06-29 14:33:29 +0200

    Add WhatsApp floating button

--- src/app/[lang]/ClientLayout.tsx
+++ src/app/[lang]/ClientLayout.tsx
@@ -14,6 +14,7 @@ import PrivacySection from '@/components/PrivacySection'
 import Footer from '@/components/Footer'
 import ScrollFrameRenderer from '@/components/ScrollFrameRenderer'
 import PageLoader from '@/components/PageLoader'
+import WhatsAppButton from '@/components/WhatsAppButton'
 
 export default function ClientLayout({ children }: { children: React.ReactNode }) {
   const { dir } = useLanguage()
@@ -26,6 +27,7 @@ export default function ClientLayout({ children }: { children: React.ReactNode }
         <Navbar />
         {children}
         <Footer />
+        <WhatsAppButton />
       </div>
     </div>
   )
